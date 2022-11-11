1c866d0c8db1aeddba669e3fe3198e70d03b2aa9
lohi212 <suvvarilohita@gmail.com>
2022-11-11 08:00:03 +0530

    feat: render reactiveList in table

---
+++ src/components/TableLayout.js
@@ -0,0 +1,65 @@
+import { ReactiveList } from "@appbaseio/reactivesearch";
+import { Table } from "antd";
+import React from "react";
+
+const TableLayout = () => {
+  const columns = [
+    {
+      title: "Member",
+      dataIndex: "Member",
+      key: "Member",
+    },
+    {
+      title: "Provider",
+      dataIndex: "provider",
+      key: "provider",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "Service",
+      dataIndex: "Service",
+      key: "Service",
+    },
+    {
+      title: "Price",
+      dataIndex: "Price",
+      key: "Price",
+    },
+    {
+      title: "Diagnosis",
+      dataIndex: "diagnosis",
+      key: "diagnosis",
+    },
+  ];
+
+  return (
+    <div>
+      <ReactiveList
+        dataField="Member.Race"
+        componentId="result"
+        react={{
+          and: ["term", "search"],
+        }}
+        render={({ loading, error, data }) => {
+          if (loading) {
+            return <div>Fetching Results.</div>;
+          }
+          if (error) {
+            return (
+              <div>
+                Something went wrong! Error details {JSON.stringify(error)}
+              </div>
+            );
+          }
+          return <Table columns={columns} dataSource={data} />;
+        }}
+      />
+    </div>
+  );
+};
+
+export default TableLayout;
